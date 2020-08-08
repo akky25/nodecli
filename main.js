84bcc45438a94df853a35566a9d12b5bd0dcdc55
akky25 <ry.akita1991@gmail.com>
2020-08-08 13:04:11 +0900

    コマンドライン引数のオプションでgfmをon/offを指定できるように追加修正

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
-// commanderモジュールをprogramとしてインポートする
+// commanderモジュールをインポート
 const program = require("commander");
-// fsモジュールをfsオブジェクトとしてインポートする
+// fsモジュールをインポート
 const fs = require("fs");
+// markedモジュールをインポート
+const marked = require("marked");
 
-
-// コマンドライン引数からcommanderでファイルパスを取得する
+// コマンドライン引数を取得する
+program.option("--gfm", "GFMを有効にする");
 program.parse(process.args);
 const filePath = program.args[0];
 
+// コマンドライン引数のオプションを取得し、デフォルトのオプションを上書きする
+const cliOptions = {
+  gfm: false,
+  ...program.opts(),
+}
+
+// 非同期でファイルを読み込む
+// utf8でエンコードを行う
 fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
   if (err) {
     console.error(err.message);
     process.exit(1);
-    return
+    return;
   }
-  console.log(file);
+  // htmlへ変換を行う
+  // gfm(GitHub Flavored Markdown)オプションの有効無効を設定する
+  const html = marked(file, {
+    gfm: cliOptions.gfm,
+  });
+  console.log(html);
 })
